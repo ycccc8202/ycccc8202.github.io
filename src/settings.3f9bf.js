0362c2acb783d9c56604b846a5676f1ab4bc5b4f
ycccc8202 <21882889@qq.com>
2020-10-24 04:41:35 +0800

    更新

---
+++ src/settings.3f9bf.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/ver_2/preload.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"fbb78",resources:"453ca",main:"74918"}};
