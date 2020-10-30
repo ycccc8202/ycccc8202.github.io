bb9c81dd9e34ae128594eccfb888b7d95cfad533
Yip Sir <7930508+wx_504ea447c8@user.noreply.gitee.com>
2020-10-30 15:27:39 +0800

    更新机器人

--- src/settings.25fef.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/ver_2/preload.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"fbb78",resources:"453ca",main:"87fa1"}};
