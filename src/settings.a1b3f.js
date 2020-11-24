033e7883ee8b521885f0bef95e4d5e3a8d5995e0
yechun <21882889@qq.com>
2020-11-25 04:24:10 +0800

    更新人机版本

---
+++ src/settings.a1b3f.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/ver_2/preload.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"fbb78",resources:"f3f08",main:"90f9a"}};
